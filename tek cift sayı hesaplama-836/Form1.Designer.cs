64874a9aaeaa0f5dbd91deb95a22a1393a600225
ilayda yalçın <146716704+ilaydayacin@users.noreply.github.com>
2023-11-18 18:44:27 +0300

    Add files via upload

---
+++ tek cift sayı hesaplama-836/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace tek_cift_sayı_hesaplama_836
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSayi = new System.Windows.Forms.TextBox();
+            this.btnSonuc = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(15, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Sayı giriniz";
+            // 
+            // txtSayi
+            // 
+            this.txtSayi.Location = new System.Drawing.Point(138, 34);
+            this.txtSayi.Name = "txtSayi";
+            this.txtSayi.Size = new System.Drawing.Size(183, 20);
+            this.txtSayi.TabIndex = 1;
+            // 
+            // btnSonuc
+            // 
+            this.btnSonuc.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnSonuc.Location = new System.Drawing.Point(93, 98);
+            this.btnSonuc.Name = "btnSonuc";
+            this.btnSonuc.Size = new System.Drawing.Size(183, 117);
+            this.btnSonuc.TabIndex = 2;
+            this.btnSonuc.Text = "Hesapla";
+            this.btnSonuc.UseVisualStyleBackColor = true;
+            this.btnSonuc.Click += new System.EventHandler(this.btnSonuc_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(498, 325);
+            this.Controls.Add(this.btnSonuc);
+            this.Controls.Add(this.txtSayi);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSayi;
+        private System.Windows.Forms.Button btnSonuc;
+    }
+}
+
